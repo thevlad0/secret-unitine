1e0083838553fba996c0d430f1f117704bfa144c
Stefka Lyaskalieva <stefka1804@gmail.com>
2025-06-08 22:58:19 +0300

    Correctly working backend logic for messages and DB

--- php/messageRepositoryAPI.php
+++ php/messageRepositoryAPI.php
@@ -2,7 +2,7 @@
 interface MessageRepositoryAPI {
     //recipientsIds sa id-tata na To: ot new message, moje i da sa id na grupi
     //@message - new message to add 
-    public function addMessage(Message $message, array recipientsIds);
+    public function addMessage(Message $message, array $recipientsIds);
 
     /*
     @messageId -> id of message that must be removed
@@ -18,22 +18,23 @@ interface MessageRepositoryAPI {
     public function getSentMessagesOfUser(int $userId): array;
     public function getInboxOfUser(int $userId): array;
 
+    public function getMessageRecipientsIds($messageId): array;
      /*
     @messageId -> id of message that must be starred/read
     @userId -> id of current user
     @folderName = current folder{Inbox, SentMessages or Deleted}
     */
-    public function starMessage(int $messageId, int $userId, string $folderName);
+    public function changeStarredStatusOfMessage(bool $isStarred, int $messageId, int $userId, string $folderName);
+
     public function readMessage(int $messageId, int $userId, string $folderName);
 
     public function filterByStar(int $userId, string $folderName) : array;
-    public function filterByAnonimity(int $userId, string $folderName) : array;
+    public function filterByRead(bool $isRead, int $userId, string $folderName) : array;
+    public function filterByAnonimity(bool $isAnonimous, int $userId, string $folderName) : array;
     public function filterByGroup(int $groupId, int $userId, string $folderName) : array;
-    public function filterByDate(string date, int $userId, string $folderName) : array;
-    public function filterByTopic(string topic, int $userId, string $folderName) : array;
+    public function filterByDate(string $date, int $userId, string $folderName) : array;
+    public function filterByTopic(string $topic, int $userId, string $folderName) : array;
 
-    public function sortAscendingByDate(int $userId, string $folderName) : array;
-    public function sortDescendingByDate(int $userId, string $folderName) : array;
-
+    public function sortMessagesByDate(string $order, int $userId, string $folderName) : array;
 }
 ?>
